799119d847c4d47e949ed4fbd78db2d2a344c6df
Yichao Yu <yyc1992@gmail.com>
2015-11-14 21:00:09 -0500

    Add test for multiple finalizers

--- test/core.jl
+++ test/core.jl
@@ -1890,7 +1890,7 @@ type ObjMember
     member::DateRange6387
 end
 
-obj = ObjMember(DateRange6387{Int64}())
+obj6387 = ObjMember(DateRange6387{Int64}())
 
 function v6387{T}(r::Range{T})
     a = Array(T,1)
@@ -1903,7 +1903,7 @@ function day_in(obj::ObjMember)
     @test isa(x, Vector{Date6387{Int64}})
     @test isa(x[1], Date6387{Int64})
 end
-day_in(obj)
+day_in(obj6387)
 
 # issue #6784
 @test ndims(Array(Array{Float64},3,5)) == 2
@@ -3901,4 +3901,24 @@ let
     arrayset_unknown_dim(Int, 1)
     arrayset_unknown_dim(Int, 2)
     arrayset_unknown_dim(Int, 3)
+end
+
+# check if we can run multiple finalizers at the same time
+let
+    # disable GC to make sure no collection/promotion happens
+    # when we are constructing the objects
+    gc_enabled13995 = gc_enable(false)
+    obj13995 = Ref(1)
+    finalized13995 = [false, false, false, false]
+    finalizer(obj13995, (x)->(finalized13995[1] = true))
+    finalizer(obj13995, (x)->(finalized13995[2] = true))
+    finalizer(obj13995, (x)->(finalized13995[3] = true))
+    finalizer(obj13995, (x)->(finalized13995[4] = true))
+    obj13995 = nothing
+    gc_enable(true)
+    # obj is unreachable and young, a single young gc should collect it
+    # and trigger all the finalizers.
+    gc(false)
+    println(finalized13995 == [true, true, true, true])
+    gc_enable(gc_enabled13995)
 end
