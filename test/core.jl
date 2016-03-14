590f8d4754609643b7fe640483aa40a4f7d14d8e
Jameson Nash <vtjnash@gmail.com>
2016-03-14 13:30:34 -0400

    add test for return in try finally
    close #1784

--- test/core.jl
+++ test/core.jl
@@ -3902,3 +3902,17 @@ let
     arrayset_unknown_dim(Int, 2)
     arrayset_unknown_dim(Int, 3)
 end
+
+# issue #1784
+let a = [false]
+function foo1784()
+    (a,b) = try
+        return true
+        (0,1)
+    finally
+         a[1] = true
+    end
+end
+@test foo1784()
+@test a[1] == true
+end
